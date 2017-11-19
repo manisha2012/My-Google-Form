113c4fd299124b8d9a32f86215cf12be4b182eac
manisha2012 <manishaa2012@gmail.com>
2017-11-19 22:35:45 +0530

    Update urls.py

--- GoogleForm/urls.py
+++ GoogleForm/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import url
-
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^signup/$', views.signup, name='signup'),
     url(r'^$', views.index, name='index'),
     url(r'^(?P<form_id>[0-9]+)/$', views.formDetail, name='formDetail'),
     url(r'^createForm/$', views.createForm, name='createForm'),
